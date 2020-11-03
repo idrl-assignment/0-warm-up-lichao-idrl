edaad6101e85fc828443d121fa43328a00542573
lichao-idrl <73737785+lichao-idrl@users.noreply.github.com>
2020-11-03 21:32:08 +0800

    Update plot_matrix.py

--- plot_matrix.py
+++ plot_matrix.py
@@ -3,16 +3,16 @@ from PIL import Image# TODO: import ...
 
 
 def generate_random_matrix(m, n):
-    matrix=np.random.randint(0,2(m,n))
+    matrix=np.random.randint(0,2,(m,n))
     return matrix
 
 
 def save_matrix(matrix, file_name):
     im=Image.fromarray(matrix)
-    im.save("example.jpg")
+    im.save("example.png")
     return
 
 
 if __name__ == "__main__":
     matrix = generate_random_matrix(10, 10)
-    save_matrix(matrix, "example.jpg")
+    save_matrix(matrix, "example.png")
